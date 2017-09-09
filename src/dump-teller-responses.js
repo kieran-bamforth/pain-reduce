3b9fb033d632556f1d6c60acc4c4ec645db36d55
Kieran Bamforth <hello@kieranbamforth.me>
2017-09-09 17:00:43 +0100

    will aws stop being a wee bitch?

--- src/dump-teller-responses.js
+++ src/dump-teller-responses.js
@@ -23,11 +23,15 @@ function dumpTellerResponses(auth, bucket, keyName) {
   }
 }
 
-[
-  {account: "55d6ff1e-f636-42a3-a58f-c6f24644f326", resource: "/standing_orders" },
-  {account: "55d6ff1e-f636-42a3-a58f-c6f24644f326", resource: "/direct_debits" },
-  {account: "f7d31ed8-c8a3-41e4-8855-2668161921ac", resource: "/standing_orders" },
-  {account: "f7d31ed8-c8a3-41e4-8855-2668161921ac", resource: "/direct_debits" },
-  {account: "78eabbb2-de1c-45a2-b586-384b063ae9dd", resource: "/standing_orders" },
-  {account: "78eabbb2-de1c-45a2-b586-384b063ae9dd", resource: "/direct_debits" }
-].forEach(dumpTellerResponses(process.env.AUTH, process.env.BUCKET, new Date().toISOString()));
+module.exports = {
+  index: function index () {
+    [
+      {account: "55d6ff1e-f636-42a3-a58f-c6f24644f326", resource: "/standing_orders" },
+      {account: "55d6ff1e-f636-42a3-a58f-c6f24644f326", resource: "/direct_debits" },
+      {account: "f7d31ed8-c8a3-41e4-8855-2668161921ac", resource: "/standing_orders" },
+      {account: "f7d31ed8-c8a3-41e4-8855-2668161921ac", resource: "/direct_debits" },
+      {account: "78eabbb2-de1c-45a2-b586-384b063ae9dd", resource: "/standing_orders" },
+      {account: "78eabbb2-de1c-45a2-b586-384b063ae9dd", resource: "/direct_debits" }
+    ].forEach(dumpTellerResponses(process.env.AUTH, process.env.BUCKET, new Date().toISOString()));
+  }
+}

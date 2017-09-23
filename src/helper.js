ce50076f2b5d34e703878d8f7919cf5a5a2d485a
Kieran Bamforth <hello@kieranbamforth.me>
2017-09-23 11:01:08 +0100

    linting the helper.js file

--- src/helper.js
+++ src/helper.js
@@ -1,30 +1,26 @@
-let request = require('request');
-let aws = require('aws-sdk');
-let s3 = new aws.S3();
-
 module.exports = {
   getKeyPath: function getKeyPath(key) {
     if (typeof key !== 'string') {
-      throw 'expected key to be a string, but got ${typeof key}';
+      throw Error(`expected key to be a string, but got ${typeof key}`);
     }
     if (key === '') {
-      throw 'expected key to not be a blank string';
+      throw Error('expected key to not be a blank string');
     }
     return key.split('/').slice(0, -1).join('/');
   },
   getKeyName: function getKeyName(key) {
     if (typeof key !== 'string') {
-      throw 'expected key to be a string, but got ${typeof key}';
+      throw Error(`expected key to be a string, but got ${typeof key}`);
     }
     if (key === '') {
-      throw 'expected key to not be a blank string';
+      throw Error('expected key to not be a blank string');
     }
     return key.split('/').slice(-1).join();
   },
   getObjectModifiedBefore: function getObjectModifiedBefore(date, objects) {
-    let result = objects.reduce((acc, current) => {
+    const result = objects.reduce((acc, current) => {
       return (current.LastModified < date && current > acc.LastModified) ? current : acc;
-    }, { LastModified: ''});
+    }, { LastModified: '' });
     if (result.LastModified === '') {
       throw new Error(`could not find any objects modified before ${date}`);
     }

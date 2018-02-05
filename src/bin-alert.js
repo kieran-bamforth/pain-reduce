7854b9e5c1281fec5295070000c4f941135e6117
Kieran Bamforth <hello@kieranbamforth.me>
2018-02-05 22:12:08 +0000

    fixing bin-alert.

--- src/bin-alert.js
+++ src/bin-alert.js
@@ -1,7 +1,6 @@
 const aws = require('aws-sdk');
 const bluebird = require('bluebird');
 const request = require('request-promise');
-const helper = request('./helper.js');
 
 aws.config.setPromisesDependency(bluebird);
 
@@ -11,16 +10,18 @@ module.exports = {
   binAlert: function binAlert(event, context, callback) {
     const postCode=  process.env.POST_CODE;
     const propertyRefNo = process.env.PROPERTY_REF_NO;
-    const emailAddress = process.env.EMAIL_ADDRESS;
-
     const options = {
       method: 'GET',
       url: `https://www.oldham.gov.uk/bin-collection-days?postcode=${postCode}&uprn=${propertyRefNo}`,
     };
 
     request(options).then((result) => {
+      const helper = require('./helper.js')
+      const emailAddress = process.env.EMAIL_ADDRESS;
+
       timetable = helper.extractBinTimetable(result);
-      parsedTimetable = helper.parsedTimetable(timetable);
+      parsedTimetable = helper.parseBinTimetable(timetable);
+
       return ses.sendEmail({
         Destination: {
           ToAddresses: [emailAddress],
@@ -29,7 +30,7 @@ module.exports = {
           Body: {
             Html: {
               Charset: 'UTF-8',
-              Data: timetable,
+              Data: timetable
             },
           },
           Subject: {

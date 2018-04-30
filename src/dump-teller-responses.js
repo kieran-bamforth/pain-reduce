b71ebce06e75de962e882a46677e2581ddbb078c
Kieran Bamforth <hello@kieranbamforth.me>
2018-04-30 14:53:38 +0100

    Updating linting for dump-teller-responses.

--- src/dump-teller-responses.js
+++ src/dump-teller-responses.js
@@ -1,16 +1,17 @@
-let request = require('request');
-let aws = require('aws-sdk');
-let s3 = new aws.S3();
+const request = require('request');
+const aws = require('aws-sdk');
+
+const s3 = new aws.S3();
 
 function dumpTellerResponses(auth, bucket, keyName) {
-  return function(opts) {
-    let url = `https://api.teller.io/accounts/${opts.account}${opts.resource}`;
-    let key = `teller-responses/${opts.account}${opts.resource}/${keyName}`;
+  return function (opts) {
+    const url = `https://api.teller.io/accounts/${opts.account}${opts.resource}`;
+    const key = `teller-responses/${opts.account}${opts.resource}/${keyName}`;
 
-    let options = {
+    const options = {
       method: 'GET',
       url: url,
-      headers: { authorization: auth }
+      headers: { authorization: auth },
     };
 
     request(options, (error, response, body) => {
@@ -20,16 +21,16 @@ function dumpTellerResponses(auth, bucket, keyName) {
         if (error) throw new Error(error);
       });
     });
-  }
+  };
 }
 
 module.exports = {
-  index: function index () {
+  index: function index() {
     [
-      {account: "f7d31ed8-c8a3-41e4-8855-2668161921ac", resource: "/standing_orders" },
-      {account: "f7d31ed8-c8a3-41e4-8855-2668161921ac", resource: "/direct_debits" },
-      {account: "78eabbb2-de1c-45a2-b586-384b063ae9dd", resource: "/standing_orders" },
-      {account: "78eabbb2-de1c-45a2-b586-384b063ae9dd", resource: "/direct_debits" }
+      { account: 'f7d31ed8-c8a3-41e4-8855-2668161921ac', resource: '/standing_orders' },
+      { account: 'f7d31ed8-c8a3-41e4-8855-2668161921ac', resource: '/direct_debits' },
+      { account: '78eabbb2-de1c-45a2-b586-384b063ae9dd', resource: '/standing_orders' },
+      { account: '78eabbb2-de1c-45a2-b586-384b063ae9dd', resource: '/direct_debits' }
     ].forEach(dumpTellerResponses(process.env.AUTH, process.env.BUCKET, new Date().toISOString()));
   }
 }
